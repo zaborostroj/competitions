46cc9d6add9108937782f136eec9fd35940ddaea
evgeny.baskakov <evgeny.baskakov@simbirsoft.com>
2019-10-18 16:28:44 +0400

    Spring security

---
+++ src/main/kotlin/com/zaborostroj/competitions/config/SecurityConfig.kt
@@ -0,0 +1,9 @@
+package com.zaborostroj.competitions.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig : WebSecurityConfigurerAdapter ()
